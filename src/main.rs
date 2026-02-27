10543c018fde56941fbaf387169070caa8b4076e
AubinFraccaro <github@sl.g4te.org>
2026-02-27 10:42:45 +0000

    initial commit : feature : add item to the shopping list

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+use std::io;
+
+
+fn add_item(new_item : &str, shop_list: &mut Vec<String>){
+    shop_list.push(new_item.to_string());
+    println!("Added item: {} to the list", new_item);
+}
+
+
+fn main() {
+    println!("--- Shoping List ---");
+    let mut shop_list: Vec<String> = Vec::new();
+
+    println!("Select an option:");
+    println!("1. Add a item to the list");
+    println!("2. Remove a item from the list");
+    println!("3. Print Shopping List");
+    println!("4. Exit");
+
+    let mut choice = String::new();
+    io::stdin()
+        .read_line(&mut choice)
+        .expect("Failed to read line");
+
+    match choice.trim() {
+        "1" =>{
+            println!("Type the item you want to add:");
+            let mut new_item = String::new();
+            io::stdin()
+            .read_line(&mut new_item)
+            .expect("Failed to read line");
+
+            add_item(new_item.trim(), &mut shop_list);
+
+        } ,
+        "2" => {},
+        "3" => {},
+        "4" => {},
+         _ => {
+             println!("Invalid choice");
+             return;
+         }
+    }
+
+
+}
