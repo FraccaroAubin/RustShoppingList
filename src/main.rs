029ca69027b8cae1b2ed30393f9a585604e42b08
AubinFraccaro <github@sl.g4te.org>
2026-02-27 15:37:55 +0000

    fix: feat : print shopping list : added fn call for print_shopping_list; took shop_list declaration out of the main loop; changed &mut param of print_shopping_list for simple & ref

--- src/main.rs
+++ src/main.rs
@@ -16,54 +16,56 @@ fn remove_item(selection: &str, shop_list: &mut Vec<String>){
     }
 }
 
-fn print_shopping_list(shop_list: &mut Vec<String>){
+fn print_shopping_list(shop_list: &Vec<String>){
     for(index, item) in shop_list.iter().enumerate(){
-        println!("{}: {} in shopping_list",index+1, item);
+        println!("{}: {} ",index+1, item);
     }
 }
 
 fn main() {
-    println!("--- Shoping List ---");
+
     let mut shop_list: Vec<String> = Vec::new();
+    
+    loop{
+        println!("--- Shoping List ---");
 
-    println!("Select an option:");
-    println!("1. Add a item to the list");
-    println!("2. Remove a item from the list");
-    println!("3. Print Shopping List");
-    println!("4. Exit");
+        println!("Select an option:");
+        println!("1. Add a item to the list");
+        println!("2. Remove a item from the list");
+        println!("3. Print Shopping List");
+        println!("4. Exit");
 
-    let mut choice = String::new();
-    io::stdin()
-        .read_line(&mut choice)
-        .expect("Failed to read line");
-
-    match choice.trim() {
-        "1" =>{
-            println!("Type the item you want to add:");
-            let mut new_item = String::new();
-            io::stdin()
-            .read_line(&mut new_item)
+        let mut choice = String::new();
+        io::stdin()
+            .read_line(&mut choice)
             .expect("Failed to read line");
 
-            add_item(new_item.trim(), &mut shop_list);
+        match choice.trim() {
+            "1" =>{
+                println!("Type the item you want to add:");
+                let mut new_item = String::new();
+                io::stdin()
+                    .read_line(&mut new_item)
+                    .expect("Failed to read line");
 
-        } ,
-        "2" => {
-            println!("Type the item you want to remove:");
-            let mut selection = String::new();
-            io::stdin()
-                .read_line(&mut selection)
-                .expect("Failed to read line");
+                add_item(new_item.trim(), &mut shop_list);
 
-            remove_item(selection.trim(), &mut shop_list);
-        },
-        "3" => {},
-        "4" => {},
-         _ => {
-             println!("Invalid choice");
-             return;
-         }
+            } ,
+            "2" => {
+                println!("Type the item you want to remove:");
+                let mut selection = String::new();
+                io::stdin()
+                    .read_line(&mut selection)
+                    .expect("Failed to read line");
+
+                remove_item(selection.trim(), &mut shop_list);
+            },
+            "3" => {print_shopping_list(&shop_list);},
+            "4" => {},
+            _ => {
+                println!("Invalid choice");
+                return;
+            }
+        }
     }
-
-
 }
